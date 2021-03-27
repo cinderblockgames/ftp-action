750a1f67d97e6fb8daf6c86d68bfb837b91b63f5
Kevin A <zimdanen@gmail.com>
2021-03-27 14:17:48 -0400

    Modified console outputs.

--- CinderBlockGames.GitHub.Actions.Ftp/Program.cs
+++ CinderBlockGames.GitHub.Actions.Ftp/Program.cs
@@ -19,24 +19,24 @@ namespace CinderBlockGames.GitHub.Actions.Ftp
         private static void Run(Options options)
         {
             // Get source files info.
-            Console.WriteLine("Finding source files...");
+            Console.WriteLine("...Finding source files...");
             var source = Directory.GetFiles(options.SourcePath, "*", SearchOption.AllDirectories)
                                   .Select(src => new Item(src, options.SourcePath, File.GetLastWriteTime(src).ToUniversalTime()));
 
             using (var client = new FtpClient(options.Server, options.Port, options.Username, options.Password))
             {
-                Console.WriteLine("Connecting to remote server...");
+                Console.WriteLine("...Connecting to remote server...");
                 client.Connect();
 
                 // Get destination files info.
-                Console.WriteLine("Finding destination files...");
+                Console.WriteLine("...Finding destination files...");
                 var destination = client.GetListing(options.DestinationPath, FtpListOption.Recursive)
                                         .Where(dest => dest.Type == FtpFileSystemObjectType.File)
                                         .Select(dest => new Item(dest.FullName, options.DestinationPath, dest.Modified));
 
                 // Delete any files that don't exist in source.
                 var delete = destination.Except(source, ItemComparer.Default);
-                Console.WriteLine($"Deleting {delete.Count()} files:");
+                Console.WriteLine($"...Deleting {delete.Count()} files...");
                 foreach (var file in delete)
                 {
                     Console.Write(file.FullPath);
@@ -50,13 +50,13 @@ namespace CinderBlockGames.GitHub.Actions.Ftp
                               where ItemComparer.Default.Equals(src, dest)
                                  && src.Modified > dest.Modified
                               select src);
-                Console.WriteLine($"Updating {update.Count()} files:");
+                Console.WriteLine($"...Updating {update.Count()} files...");
                 Upload(client, update);
                 Console.WriteLine();
 
                 // Upload any files that are new.
                 var upload = source.Except(destination, ItemComparer.Default);
-                Console.WriteLine($"Uploading {upload.Count()} new files:");
+                Console.WriteLine($"...Uploading {upload.Count()} new files...");
                 Upload(client, upload);
                 Console.WriteLine();
 
@@ -69,7 +69,7 @@ namespace CinderBlockGames.GitHub.Actions.Ftp
             var grouped = files.GroupBy(file => file.Directory, StringComparer.OrdinalIgnoreCase);
             foreach (var kvp in grouped)
             {
-                Console.WriteLine($"Directory: {kvp.Key}");
+                Console.WriteLine($"[Directory: {kvp.Key}]");
                 Console.WriteLine(string.Join(Environment.NewLine, kvp.Select(file => file.FullPath)));
                 client.UploadFiles(kvp.Select(file => file.FullPath), kvp.Key);
             }
